f4d9027017a2d89bae747fcd8a08909f9ea2f557
ian <igf1002@plymouth.edu>
2024-12-02 10:59:54 -0500

    Add energy pies and max values

--- sim.js
+++ sim.js
@@ -1,18 +1,5 @@
-const G = 9.8;
-
-let moving = false;
-let offset = -1;
-
-function F_g(m, g) { return m * g; }
-function F_spring(k, d) { return -k * d; }
-function gravitational_energy(m, g, h) { return m * g * h; }
-function kinetic_energy(m, v) { return 0.5 * m * v * v; }
-function elastic_energy(k, d) { return 0.5 * k * d * d; }
-
-function horizontal_spring(start, end, y, width, turns)
+function horizontal_spring(start, end, y, width, turns, equilibrium)
 {
-    console.log(start + " -> " + end);
-
     step = (end - start) / turns;
 
     for (i = 0; i < turns; ++i) {
@@ -20,36 +7,111 @@ function horizontal_spring(start, end, y, width, turns)
 	line(start + i * step, y + width, start + (i + 1) * step, y - width);
     }
 
+    stroke("red");
+    line(equilibrium, 50, equilibrium, 150);
+
+    stroke("black");
     circle(end, y, 40);
 }
 
-function mousePressed(event) { offset = displacement - mouseX; moving = true; }
-function mouseReleased(event) { moving = false; }
+function vertical_spring(start, end, x, width, turns, equilibrium)
+{
+    step = (end - start) / turns;
+
+    for (i = 0; i < turns; ++i) {
+	line(x - width, start + i * step, x + width, start + (i + 1) * step);
+	line(x + width, start + i * step, x - width, start + (i + 1) * step);
+    }
+
+    stroke("red");
+    line(50, equilibrium, 150, equilibrium);
+
+    stroke("black");
+    circle(x, end, 40);
+}
+
+function F_g(m, g) { return m * g; }
+function F_spring(k, d) { return -k * d; }
+function E_g(m, g, h) { return m * g * h; }
+function E_k(m, v) { return 0.5 * m * v * v; }
+function E_el(k, d) { return 0.5 * k * d * d; }
+function equilibrium_position(k, m, g) { return (m * g) / k; }
+
+function mousePressed(event)
+{
+    offset = springs[0].weight.displacement - mouseX;
+    moving = true;
+}
+
+function mouseReleased(event)
+{
+    moving = false;
+}
+
+class Weight {
+    constructor(mass, displacement = 0, velocity = 0, acceleration = 0) {
+	this.mass = mass;                 /* g(?) */
+	this.displacement = displacement; /* meters */
+	this.velocity = velocity;         /* m/s */
+	this.acceleration = acceleration; /* m/s^2 */
+
+	this.max_d = this.max_v = this.max_a = 0;
+    }
+}
+
+class Spring {
+    constructor(stiffness, weight, length) {
+	this.weight = weight;
+	this.stiffness = stiffness; /* N/m */
+	this.equilibrium = length;  /* meters */
+    }
+}
+
+const G = 9.8;
+
+moving = false;
+offset = -1;
 
 function setup()
 {
     createCanvas(windowWidth, windowHeight);
+    springs = [new Spring(2, new Weight(100, 50), 300)];
+    total_energy = E_el(2, 50);
 
-    spring_constant = 0.5; /* N/m */
-    mass = 100;          /* kg */
-    velocity = 0;        /* M/s */
-    displacement = 0;    /* meters */
-    equilibrium = 200;   /* meters */
+    // k, (m, d, v, a), e
+//    springs = [new Spring(2, new Weight(100, 30), 100)];
 }
 
 function draw()
 {
-    net_force = F_spring(spring_constant, displacement);
-    acceleration = net_force / mass;
-    velocity += acceleration;
-    displacement += velocity;
+    for (s of springs) {
+	net_force = F_spring(s.stiffness, s.weight.displacement);
+	s.weight.acceleration = net_force / s.weight.mass;
+	s.weight.velocity += s.weight.acceleration;
+	s.weight.displacement += s.weight.velocity;
 
-    if (moving) {
-	displacement = offset + mouseX;
-	acceleration = velocity = 0;
-	console.log("offset: " + offset);
+	s.weight.max_d = max(s.weight.max_d, s.weight.displacement);
+	s.weight.max_v = max(s.weight.max_v, s.weight.velocity);
+	s.weight.max_a = max(s.weight.max_a, s.weight.acceleration);
+
+	if (moving) {
+	    s.weight.displacement = offset + mouseX;
+	    s.weight.acceleration = s.weight.velocity = 0;
+	}
+
+	background(200);
+
+	p_eel = min(E_el(s.stiffness, s.weight.displacement) / total_energy, 1);
+	p_ek = min(E_k(s.weight.mass, s.weight.velocity) / total_energy, 1);
+
+	arc(500, 500, 100, 100, 0, 2 * PI * p_eel);
+	arc(650, 500, 100, 100, 0, 2 * PI * p_ek);
+
+	text("(" + s.weight.max_d + ") d = " + s.weight.displacement, 200, 200);
+	text("(" + s.weight.max_v + ") v = " + s.weight.velocity, 200, 250);
+	text("(" + s.weight.max_a + ") a = " + s.weight.acceleration, 200, 225);
+
+	horizontal_spring(0, s.equilibrium + s.weight.displacement, 100, 20, 10, s.equilibrium);
+	//    vertical_spring(0, equilibrium + displacement, 100, 20, 10, equilibrium);
     }
-
-    background(200);
-    horizontal_spring(0, equilibrium + displacement, 100, 20, 10);
 }
